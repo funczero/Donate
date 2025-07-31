2322f13aa9afc6b975395838ea45df83928ef988
FuncZero <contato@funczero.xyz>
2025-07-31 20:33:21 -0300

    style(logger): organiza configuração do Winston Logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -4,15 +4,15 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.printf(({ level, message, timestamp }) =>
-      `[${timestamp}] [${level.toUpperCase()}] ${message}`
-    )
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] [${level.toUpperCase()}] ${message}`;
+    })
   ),
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' })
-  ]
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
 });
 
 export default logger;
